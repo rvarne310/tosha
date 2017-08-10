30af54a2f52f1ad560ef700747347983c56fbc72
Avinash Lakhmawad <avipl1994@gmail.com>
2017-08-11 00:38:07 +0530

    web page created

--- templates/script.js
+++ templates/script.js
@@ -1,22 +1,27 @@
 ﻿var app = angular.module("myApp", []);
 
 app.config(function($interpolateProvider) {
-        $interpolateProvider.startSymbol('//').endSymbol('//');
-    });
+    $interpolateProvider.startSymbol('//').endSymbol('//');
+});
 
-var update = function ($scope, $http) {
-    console.log("fine");
-    $scope.test = "OK";
+var showTrackDetails = function ($rootScope, $scope, $http) {
 
-    $http({
-        method: 'POST',
-        url: '/update',
-    }).then(function (response) {
-        var JSONData = response.data;
-        $scope.msg = JSONData["speech"];
-    }, function (error) {
-        console.log(error);
-    });
+    $rootScope.show = false;
+    $scope.showDetailsClicked = function () {
+        console.log("ok");
+        $http({
+            method: 'POST',
+            url: '/update',
+        }).then(function (response) {
+            var JSONData = response.data;
+            $scope.msg = JSONData["speech"];
+            if ($scope.msg == "Sample speech") {
+                $rootScope.show = true;
+            }
+        }, function (error) {
+            console.log(error);
+        });
+    }
 }
 
-app.controller("updateCntrl", update);
+app.controller("showTrackDetails", showTrackDetails);
