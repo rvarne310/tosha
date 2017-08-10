c69dc73e7c90307e918ba8b2532653ff02520d7e
Avinash Lakhmawad <avipl1994@gmail.com>
2017-08-10 08:22:26 +0530

    debug-1

--- templates/script.js
+++ templates/script.js
@@ -1,14 +1,19 @@
 ﻿var app = angular.module("myApp", []);
 
+app.config(function($interpolateProvider) {
+        $interpolateProvider.startSymbol('//').endSymbol('//');
+    });
+
 var update = function ($scope, $http) {
     console.log("fine");
     $scope.test = "OK";
+
     $http({
         method: 'POST',
         url: '/update',
     }).then(function (response) {
         var JSONData = response.data;
-        $scope.msg = JSONData;
+        $scope.msg = JSONData["speech"];
     }, function (error) {
         console.log(error);
     });
